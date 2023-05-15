41a46c9e459e6e0e0b9df0b97f61f184910df9de
Cosmas Langat <cosmas.lagat@APOLLOGROUP.CO.KE>
2023-05-15 11:09:39 +0300

    Initial commit

---
+++ src/test/kotlin/com/quotation/quotation/QuotationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quotation.quotation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuotationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
