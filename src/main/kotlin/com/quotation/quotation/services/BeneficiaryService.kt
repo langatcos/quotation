0bd9d310e75505d31c56522c8b25996fa713a7e6
cosmas.lagat <pkiruicos@gmail.com>
2023-05-15 11:52:45 +0300

    Added API Operation for Swagger documentation

--- src/main/kotlin/com/quotation/quotation/services/BeneficiaryService.kt
+++ src/main/kotlin/com/quotation/quotation/services/BeneficiaryService.kt
@@ -2,6 +2,7 @@ package com.quotation.quotation.services
 
 import com.quotation.quotation.dataclasses.Beneficiary
 import com.quotation.quotation.repositories.BeneficiaryRepository
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 class BeneficiaryService (val beneficiaryRepository: BeneficiaryRepository) {
     @CrossOrigin
     @GetMapping("getallbeneficiaries")
+    @ApiOperation(value="Get All Beneficiaries",notes="Get All Beneficiaries")
      fun getAllBen():List<Beneficiary>{
          return beneficiaryRepository.getAllBeneficiaries()
      }
