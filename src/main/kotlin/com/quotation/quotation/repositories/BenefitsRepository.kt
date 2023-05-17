653ecc9020ce2d5904485a5a43957855c8c60f9b
Cosmas <pkiruicos@gmail.com>
2023-05-17 14:37:48 +0300

    Adding Category to the the Benefit Class

--- src/main/kotlin/com/quotation/quotation/repositories/BenefitsRepository.kt
+++ src/main/kotlin/com/quotation/quotation/repositories/BenefitsRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 
 interface BenefitsRepository:CrudRepository<Benefits,String> {
-    @Query("select * from benefits order by benefitid asc")
+    @Query("select * from benefits order by description asc")
     fun getAllBenefits():List<Benefits>
     fun getAllBenefitsByProductid(productid:Int):List<Benefits>
     fun getAllBenefitsByCategory(category:String):List<Benefits>
